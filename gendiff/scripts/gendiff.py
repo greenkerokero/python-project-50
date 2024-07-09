acfb488ae8ada1e0728b795f55246a468bbb7f43
Dmitriy <kerokero@yandex.ru>
2024-07-09 12:17:46 +0000

    change help output for gendiff script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,10 +5,12 @@ from gendiff.makediff import generate_diff
 
 def main():
     parser = argparse.ArgumentParser(
+        prog='gendiff',
+        usage='%(prog)s [options] <filepath1> <filepath2>',
         description='Compares two configuration files and shows a difference.',
     )
-    parser.add_argument('first_file', help='path to first file')
-    parser.add_argument('second_file', help='path to second file')
+    parser.add_argument('first_file', help=argparse.SUPPRESS)
+    parser.add_argument('second_file', help=argparse.SUPPRESS)
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     compare = generate_diff(args.first_file, args.second_file)
