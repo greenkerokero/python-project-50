0230a20e01f8a31b01bd2d4c74ed31aebe62dac8
Dmitriy <kerokero@yandex.ru>
2024-12-02 08:06:51 +0000

    add build_diff_tree function in makediff module

--- gendiff/makediff.py
+++ gendiff/makediff.py
@@ -7,6 +7,22 @@ outputs the difference between them as string.
 from gendiff.parsing import get_data
 
 
+def get_file_extension(path):
+    """Get file extension from path to file.
+
+    Args:
+        path: Path to file
+
+    Returns:
+        String with extension of file. If extension not exist - empty string
+    """
+    file_name = path.split('/')[-1]
+    split_file_name = file_name.split('.')
+    if len(split_file_name) < 2:
+        return ''
+    return split_file_name[-1]
+
+
 def bool_to_lowercase(input_value):
     """If input value is boolean convert it to string in lowercase.
 
@@ -21,6 +37,42 @@ def bool_to_lowercase(input_value):
     return input_value
 
 
+def build_diff_tree(first_dict, second_dict):
+    def inner(first, second):
+        inner_repr = {}
+        keys = first.keys() | second.keys()
+        keys = sorted(keys)
+        for key in keys:
+            if key not in first:
+                inner_repr[key] = {
+                    'type': 'added',
+                    'value': second[key],
+                }
+            elif key not in second:
+                inner_repr[key] = {
+                    'type': 'deleted',
+                    'value': first[key],
+                }
+            elif first[key] == second[key]:
+                inner_repr[key] = {
+                    'type': 'unchanged',
+                    'value': first[key],
+                }
+            elif isinstance(first[key], dict) and isinstance(second[key], dict):
+                inner_repr[key] = {
+                    'type': 'nested',
+                    'value': inner(first[key], second[key]),
+                }
+            else:
+                inner_repr[key] = {
+                    'type': 'changed',
+                    'old': first[key],
+                    'new': second[key],
+                }
+        return inner_repr
+    return inner(first_dict, second_dict)
+
+
 def make_diff(file_path1, file_path2):
     """Generate difference between two json or yaml files.
 
@@ -32,24 +84,33 @@ def make_diff(file_path1, file_path2):
         String contains difference between two files located at paths
         in file_path1 and file_path2
     """
-    first_data, second_data = get_data(file_path1, file_path2)
-    keys = first_data.keys() | second_data.keys()
+    with open(file_path1, 'r') as first_file:
+        first_list = [line for line in first_file]
+        first_data = ''.join(first_list)
+    with open(file_path2, 'r') as second_file:
+        second_list = [line for line in second_file]
+        second_data = ''.join(second_list)
+    extention = get_file_extension(file_path1)
+    first_dict = get_data(first_data, extention)
+    second_dict = get_data(second_data, extention)
+
+    keys = first_dict.keys() | second_dict.keys()
     keys = sorted(keys)
     diff_list = ['{']
     for key in keys:
-        if key not in first_data:
-            second_data_value = bool_to_lowercase(second_data[key])
-            diff_list.append(f'  + {key}: {second_data_value}')
-        elif key not in second_data:
-            first_data_value = bool_to_lowercase(first_data[key])
-            diff_list.append(f'  - {key}: {first_data_value}')
-        elif first_data[key] == second_data[key]:
-            first_data_value = bool_to_lowercase(first_data[key])
-            diff_list.append(f'    {key}: {first_data_value}')
+        if key not in first_dict:
+            second_dict_value = bool_to_lowercase(second_dict[key])
+            diff_list.append(f'  + {key}: {second_dict_value}')
+        elif key not in second_dict:
+            first_dict_value = bool_to_lowercase(first_dict[key])
+            diff_list.append(f'  - {key}: {first_dict_value}')
+        elif first_dict[key] == second_dict[key]:
+            first_dict_value = bool_to_lowercase(first_dict[key])
+            diff_list.append(f'    {key}: {first_dict_value}')
         else:
-            first_data_value = bool_to_lowercase(first_data[key])
-            second_data_value = bool_to_lowercase(second_data[key])
-            diff_list.append(f'  - {key}: {first_data_value}')
-            diff_list.append(f'  + {key}: {second_data_value}')
+            first_dict_value = bool_to_lowercase(first_dict[key])
+            second_dict_value = bool_to_lowercase(second_dict[key])
+            diff_list.append(f'  - {key}: {first_dict_value}')
+            diff_list.append(f'  + {key}: {second_dict_value}')
     diff_list.append('}')
     return '\n'.join(diff_list)
