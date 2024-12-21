3c1d2a4dfd5d9976303e904605aae97301b4809b
Dmitriy <kerokero@yandex.ru>
2024-12-21 17:06:44 +0000

    update help output for script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Entry point of project. Script implement user interface."""
 import argparse
+import sys
 
 from gendiff.generate_diff import generate_diff
 
@@ -8,19 +9,25 @@ from gendiff.generate_diff import generate_diff
 def main():
     """Implement user interface."""
     parser = argparse.ArgumentParser(
-        usage='gendiff [options] <first_file> <second_file>',
-        description='Compares two configuration files and shows a difference.',
+        usage='%(prog)s [options] FILE1 FILE2',
+        description='Compares two configuration files and shows a difference',
+        formatter_class=argparse.RawTextHelpFormatter,
     )
-    parser.add_argument('first_file', help=argparse.SUPPRESS)
-    parser.add_argument('second_file', help=argparse.SUPPRESS)
     parser.add_argument(
         '-f',
         '--format',
-        default='stylish',
-        help='set format of output',
+        help=''.join(
+            'specify the format of the input file\n'
+            'available formats: stylish (default), plain, json',
+        ),
     )
+    parser.add_argument('FILE1', help=argparse.SUPPRESS)
+    parser.add_argument('FILE2', help=argparse.SUPPRESS)
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(0)
     args = parser.parse_args()
-    compare = generate_diff(args.first_file, args.second_file, args.format)
+    compare = generate_diff(args.FILE1, args.FILE2, args.format)
     print(compare)
 
 
